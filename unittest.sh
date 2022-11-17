6aff8e9089fe20a14be68414739d64a9aae384ab
scamer300 <scamer300@caledonian.ac.uk>
2022-11-17 21:55:29 +0000

    add variable test

--- unittest.sh
+++ unittest.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
+javac CurrencyConverter.java
 
 output=$(java CurrencyConverter 1 dollars)
-expected="1.0 Dollars = 0.74 Pounds 
-1.0 Dollars = 0.88 Euros 
-Thank you for using the converter."
+expected="1.0 Dollars = 0.74 Pounds 1.0 Dollars = 0.88 Euros Thank you for using the converter."
 
 echo $output
 echo $expected
