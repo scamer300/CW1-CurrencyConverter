469044a2045eaeb6c1547696eec220a883705809
scamer300 <scamer300@caledonian.ac.uk>
2022-11-17 21:17:27 +0000

    add variable test

--- unittest.sh
+++ unittest.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-$output=$(java CurrencyConverter 1 dollars)
-$expected=$("1.0 Dollars = 0.74 Pounds" + "\n1.0 Dollars = 0.88 Euros" + "\nThank you for using the converter.")
+output=$(java CurrencyConverter 1 dollars)
+expected="1.0 Dollars = 0.74 Pounds" + "\n1.0 Dollars = 0.88 Euros" + "\nThank you for using the converter."
+
 
 if [ "output" == "expected" ]; then
     echo "Test passed!"
