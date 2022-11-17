1abad6071984f56c66eda023c94c77bce2302894
scamer300 <scamer300@caledonian.ac.uk>
2022-11-17 22:47:02 +0000

    changing variable test

--- unittestC.sh
+++ unittestC.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 javac CurrencyConverter.java
 
-actual=$(java CurrencyConverter 1 dollar)
+actual=$(java CurrencyConverter 1 dollars)
 expected=$'1.0 Dollars = 0.74 Pounds\n1.0 Dollars = 0.88 Euros\nThank you for using the converter.'
 
 echo $actual
