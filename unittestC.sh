975cb218a7f410f0befd70404cd1d9ece443fa9f
scamer300 <scamer300@caledonian.ac.uk>
2022-11-17 22:21:40 +0000

    rename unittest file to unittestC

--- unittestC.sh
+++ unittestC.sh
@@ -7,9 +7,9 @@ expected="1.0 Dollars = 0.74 Pounds 1.0 Dollars = 0.88 Euros Thank you for using
 echo $actual
 echo $expected
 
-if [[ $expected > $actual ]]; then
-    echo "Expected is larger"
+if [ "$expected" == "$actual" ]; then
+    echo "Test passed!"
 else
-    echo "Expected is smaller"
+    echo "Test failed!"
     exit 1
 fi
