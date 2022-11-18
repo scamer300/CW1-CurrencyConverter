d07379417d0e0a9a46c3b4d471a42f9cccb1512a
scamer300 <scamer300@caledonian.ac.uk>
2022-11-18 00:14:56 +0000

    added an input variable

--- unitTestDollars.sh
+++ unitTestDollars.sh
@@ -2,7 +2,7 @@
 javac CurrencyConverter.java
 
 input=$'1 dollars'
-actual=$(java CurrencyConverter input)
+actual=$(java CurrencyConverter $input)
 expected=$'1.0 Dollars = 0.74 Pounds\n1.0 Dollars = 0.88 Euros\nThank you for using the converter.'
 
 if [ "$expected" == "$actual" ]; then
