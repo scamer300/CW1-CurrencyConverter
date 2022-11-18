d7693374a76fb36c6041169074007eb367e36dd5
scamer300 <scamer300@caledonian.ac.uk>
2022-11-18 19:59:40 +0000

    adding logger

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -1,9 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.text.DecimalFormat;
 
 public class CurrencyConverter {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        PrintStream outStream1 = new PrintStream(new File("outFile.txt"));
         try {
+            PrintStream outStream = new PrintStream(new File("outFile.txt"));
 
             double amount;
             double dollar;
@@ -22,8 +28,10 @@ public class CurrencyConverter {
                     case "Dollars":
                         // For Dollar Conversion
                         pound = amount * 0.74;
+                        System.setOut(outStream);
                         System.out.println(amount + " Dollars = " + f.format(pound) + " Pounds");
                         euro = amount * 0.88;
+                        System.setOut(outStream);
                         System.out.println(amount + " Dollars = " + f.format(euro) + " Euros");
                         break;
                     case "pounds":
@@ -31,8 +39,10 @@ public class CurrencyConverter {
                     case "Pounds":
                         // For Pound Conversion
                         dollar = amount * 1.36;
+                        System.setOut(outStream);
                         System.out.println(amount + " Pounds = " + f.format(dollar) + " Dollars");
                         euro = amount * 1.19;
+                        System.setOut(outStream);
                         System.out.println(amount + " Pound = " + f.format(euro) + " Euros");
                         break;
                     case "euros":
@@ -40,17 +50,22 @@ public class CurrencyConverter {
                     case "Euros":
                         // For Euro Conversion
                         dollar = amount * 1.13;
+                        System.setOut(outStream);
                         System.out.println(amount + " Euros = " + f.format(dollar) + " Dollars");
                         pound = amount * 0.84;
+                        System.setOut(outStream);
                         System.out.println(amount + " Euros = " + f.format(pound) + " Pounds");
                         break;
                     default:
+                        System.setOut(outStream);
                         System.out.println("No input has been provided");
                 }
 
+            System.setOut(outStream);
             System.out.println("Thank you for using the converter.");
         }
         catch (ArrayIndexOutOfBoundsException e){
+            System.setOut(outStream1);
             System.out.println("No input has been provided.");
         }
     }
